567eef00148f1edc0f10749b856e16a87d55e611
Sam Cogan <mail@samcogan.com>
2024-02-20 22:03:16 +0000

    add failure message

--- arm-ttk-extension-xplatform/powershell/Export-NUnitXml.psm1
+++ arm-ttk-extension-xplatform/powershell/Export-NUnitXml.psm1
@@ -97,9 +97,10 @@ Function Export-NUnitXml {
                 if ($result.warnings.count -gt 0) {
                     $TestCase = @"
                     <test-case description="$($result.name) in template file $directoryName\$fileName" name="$($result.name) - $fileName" time="$([math]::Round($result.timespan.TotalSeconds,4).toString())" asserts="0" success="True" result="Inconclusive" executed="True">
-                            <output>
-                              <![CDATA[$($result.warnings.message)]]> in template file $fileName
-                            </output>
+                                <failure>
+        <message>   <![CDATA[$($result.warnings.message)]]> in template file $fileName</message>
+e>
+    </failure>
                     </test-case>`n
 "@
                 }
