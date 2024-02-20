a203da0fd59fbcfa0197ac5ae9ab9944d811b494
Sam Cogan <mail@samcogan.com>
2024-02-20 14:53:09 +0000

    update assert

--- arm-ttk-extension-xplatform/powershell/Export-NUnitXml.psm1
+++ arm-ttk-extension-xplatform/powershell/Export-NUnitXml.psm1
@@ -96,14 +96,13 @@ Function Export-NUnitXml {
             if ($result.Passed) {
                 if ($result.warnings.count -gt 0) {
                     $TestCase = @"
-                    <test-case description="$($result.name) in template file $directoryName\$fileName" name="$($result.name) - $fileName" time="$([math]::Round($result.timespan.TotalSeconds,4).toString())" asserts="$($results.warnings.count)" success="True" result="Success" executed="True">
+                    <test-case description="$($result.name) in template file $directoryName\$fileName" name="$($result.name) - $fileName" time="$([math]::Round($result.timespan.TotalSeconds,4).toString())" asserts="$($result.warnings.count)" success="True" result="Success" executed="True">
                         <assertions>
                             <assertion result="warning">
                                 <message><![CDATA[$($result.warnings.message)]]> in template file $fileName</message>
                             </assertion>
                         </assertions>
                     </test-case>`n
-                }
 "@
                 }
                 else {
