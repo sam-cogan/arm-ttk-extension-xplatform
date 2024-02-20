eeacf0f6cd842cd7920a435955936750225e5466
Sam Cogan <mail@samcogan.com>
2024-02-20 15:44:00 +0000

    update output

--- arm-ttk-extension-xplatform/powershell/Export-NUnitXml.psm1
+++ arm-ttk-extension-xplatform/powershell/Export-NUnitXml.psm1
@@ -98,7 +98,7 @@ Function Export-NUnitXml {
                     $TestCase = @"
                     <test-case description="$($result.name) in template file $directoryName\$fileName" name="$($result.name) - $fileName" time="$([math]::Round($result.timespan.TotalSeconds,4).toString())" asserts="0" success="True" result="Inconclusive" executed="True">
                             <output>
-                                <message><![CDATA[$($result.warnings.message)]]> in template file $fileName</message>
+                              <![CDATA[$($result.warnings.message)]]> in template file $fileName
                             </output>
                     </test-case>`n
 "@
